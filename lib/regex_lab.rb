e9373e89920b10aecf0029bb08c4c6e306269a75
Mariana Sickler <marianaleal_93@hotmail.com>
2017-09-29 18:00:16 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -12,7 +12,7 @@ text.scan(/\b[a-z]{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
- text.match(/\b[A-Z].+\w+\b./) ? true : false
+ text.scan(/\b[A-Z]+\w+\b\S\s/) ? true : false
 end
 
 def valid_phone_number?(phone)
