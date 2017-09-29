e0cf38b9e770cdbaeee4161f5f78b4f0dfb20b39
Mariana Sickler <marianaleal_93@hotmail.com>
2017-09-29 17:33:21 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -3,7 +3,8 @@ word.match(/\b[aeiou]/i) ? true : false
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+text.match(/\bun+\b.*ing\b/) 
+return text
 end
 
 def words_five_letters_long(text)
